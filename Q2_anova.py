49e99eef9f4ab2ebb32a7f5494b1a07f9d579793
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2022-01-13 14:46:23 +0800

    Read description for details.
    1. Change the distribution of the dependent variable in Q2, assuming the raw distribution fits Poisson distribution.
    2. Correct errors in ANOVA.

--- Q2_anova.py
+++ Q2_anova.py
@@ -1,28 +1,29 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import f
 
 # %% Load data.
 x, y = pd.read_pickle('raw/Q2_pre_processed.pkl')
 feature_importance = pd.read_excel('results/Q2_feature_importance.xlsx')
+transformer = pd.read_pickle('raw/Q2_mdl_pre_processed.pkl')
 
 # %% Group the dependent variable.
-days_description = {
-    1: 'Within a day', 7: 'Within a week', 14: 'Within two weeks', 30: 'Within a month', 91: 'Within a quarterly',
-    182: 'Within half a year', 365: 'More than half a year'
-}
-y_days = (1 / y - 1).values
-y_grouped = np.piecewise(y_days,
-                         condlist=[y_days <= 1, (y_days > 1) & (y_days <= 7), (y_days > 7) & (y_days <= 14),
-                                   (y_days > 14) & (y_days <= 30), (y_days > 30) & (y_days <= 91),
-                                   (y_days > 91) & (y_days <= 182)],
-                         funclist=list(days_description.keys())).astype('int32')
+y_bins = np.quantile(y, q=[0.2, 0.4, 0.6, 0.8])
+y_grouped = np.sum(y.values[:, np.newaxis] < y_bins, axis=1)
 
 # %%
 features = feature_importance.loc[feature_importance['Feature importance'] >= 0.05, 'Name'].tolist()
-anova = pd.DataFrame(columns=features, index=list(days_description.keys()))
-for period in list(days_description.keys()):
-    anova.loc[period, features] = x[features].values[y_grouped == period, :].std(axis=0) / x[features].std(axis=0)
+anova = pd.DataFrame(columns=features)
+
+totals = x[features].values
+totals_std = np.std(totals, axis=0)
+n = totals.shape[0]
+for period in range(y_bins.shape[0] + 1):
+    samples = totals[y_grouped == period, :]
+    m = samples.shape[0]
+    F = np.std(samples, axis=0) / totals_std
+    p = f.cdf(F, m-1, n-1)
+    anova.loc[period, features] = p
 
 # %%
-anova['Description'] = list(days_description.values())
 anova.to_excel('results/Q2_ANOVA.xlsx', index=False)
--- pre_processor_extended.py
+++ pre_processor_extended.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 from sklearn.base import TransformerMixin
-import json
+
 
 class LifePeriod(TransformerMixin):
     def __init__(self, push_date, pull_date, new_name, unit='days'):
@@ -8,15 +9,21 @@ class LifePeriod(TransformerMixin):
         self.pull_date = pull_date
         self.new_name = new_name
         self.unit = unit
+        self.period = 1
 
-    def fit(self, x, *args, **kwargs): return self
+    def fit(self, x, *args, **kwargs):
+        duration = (x[self.pull_date] - x[self.push_date]) / pd.to_timedelta(1, unit=self.unit)
+        self.period = np.nanmean(duration)
+        return self
 
     def transform(self, x, *args, **kwargs):
         x_ = x.copy()
-        x_.loc[x[self.pull_date].isna(), self.new_name] = 0
-        duration = (x.loc[~x[self.pull_date].isna(), self.pull_date] -
-                    x.loc[~x[self.pull_date].isna(), self.push_date]) / pd.to_timedelta(1, unit=self.unit)
-        x_.loc[~x[self.pull_date].isna(), self.new_name] = 1 / (duration + 1)
+        duration = (x[self.pull_date] - x[self.push_date]) / pd.to_timedelta(1, unit=self.unit)
+        duration[duration.isna()] = self.period + 3 * np.nanstd(duration)
+        duration = - np.log(duration / self.period)
+        duration[duration >= 3] = 3
+        duration[duration <= -3] = -3
+        x_[self.new_name] = duration
         return x_
 
 
